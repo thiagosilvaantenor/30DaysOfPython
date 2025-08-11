ee85ee37c572b107b3839308b54fbce0fc391edf
Thiago Antenor <thiagoantenor31@gmail.com>
2025-08-11 12:41:12 -0300

    feat: beginning day 9 project

---
+++ dia9/credit_card_validator.py
@@ -0,0 +1,5 @@
+# /bin/python3
+# Recevies a Credit Card number from user to validate with the Lumn algorithm
+
+original_number = str(input('Type the credit card number you want to test it '))
+print(original_number)

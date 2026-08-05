ec1fd26c9561eb77ce8c3a7cb00fc7586636583e
Thiago Antenor <thiagoantenor31@gmail.com>
2026-08-04 21:37:08 -0300

    refactor: day27 refactor project

--- dia27/project/working_with_pandas.py
+++ dia27/project/working_with_pandas.py
@@ -16,26 +16,24 @@ def get_data(file="./data/avocado.csv") -> DataFrame:
     return dt
 
 
-# Generalist function to deal with highest or lowest avg price in year
-def find_with_year(dt: DataFrame, year, function: str):
-    same_year = dt.query("year == @year")
-    if function == "low":
-        lowest = same_year["AveragePrice"].min()
-        return same_year.query("AveragePrice == @lowest")
-    else:
-        highest = same_year["AveragePrice"].max()
-        return same_year.query("AveragePrice == @highest")
+# filter dt to only the year inform, and drops the column
+def find_with_year(dt: DataFrame, year):
+    return dt.query("year == @year")
+    # if function == "low":
+    #     lowest = same_year["AveragePrice"].min()
+    #     return same_year.query("AveragePrice == @lowest")
+    # else:
+    #     highest = same_year["AveragePrice"].max()
+    #     return same_year.query("AveragePrice == @highest")
 
 
-def show_avg_price_each_year(dt, function, years):
-    if function not in ("low", "high"):
-        print('Oops, wrong argument for "function", use "low" or "high"')
-        return None
+def get_avg_price_each_year(dt, years):
 
+    averages = {}
     for year in years:
-        result = find_with_year(dt, year, function).drop_duplicates()
-        result = result[["year", "region", "type", "AveragePrice"]]
-        print(result.head())
+        result = find_with_year(dt, year).groupby("region").mean(numeric_only=True)
+        averages.update({year: result})
+    return averages
 
 
 def show_avg_price_all_time(dt, function, years):
@@ -45,7 +43,7 @@ def show_avg_price_all_time(dt, function, years):
     # Find the avg price of each year and saves in the dataframe
     dt_result = pd.DataFrame({"AveragePrice": [0.0]}, index=[0])
     for year in years:
-        result = find_with_year(dt, year, function).drop_duplicates()
+        result = find_with_year(dt, year).drop_duplicates()
         result = result[["year", "region", "type", "AveragePrice"]]
         dt_result = pd.concat([dt_result, result["AveragePrice"]])
 
@@ -58,47 +56,61 @@ def show_avg_price_all_time(dt, function, years):
         return dt.loc[dt.AveragePrice == highest["AveragePrice"]]
 
 
+def show_min_max(dt, type):
+    # which region had the lowest avg price,
+    #  organic avocados each year
+    print(f"#### Lowest AVG Price Region for {type} grown avocados ####\n")
+    years = dt["year"].drop_duplicates()
+    avgs = get_avg_price_each_year(dt, years)
+    for year in avgs:
+        lowest = avgs[year].min()["AveragePrice"]
+        result = avgs[year].query("AveragePrice == @lowest")
+        print(f"{result.head()}")
+
+    # # highest avg price for both types each year
+    # conventionally
+
+    print(f"#### Highest AVG Price Region for {type} grown avocados ####\n")
+    for year in avgs:
+        highest = avgs[year].max()["AveragePrice"]
+        result = avgs[year].query(
+            "AveragePrice == @highest",
+        )
+        print(f"{result.head()}")
+
+    print("###############\n")
+
+
 # which region had the lowest avg price,
 #  conventionally grown each year
 dt = get_data()
-
 # filter type
-dt_conv = dt.query("type == 'conventional'")
-years = dt_conv["year"].drop_duplicates()
-print("#### Lowest AVG Price Region for Convetionally grown avocados ####\n")
-show_avg_price_each_year(dt_conv, "low", years)
-print("###############\n")
-
+dt_conv = dt.query("type == 'conventional'").copy()
+show_min_max(dt_conv, "Conventionally")
 
 # which region had the lowest avg price,
 #  organic avocados each year
-print("#### Lowest AVG Price Region for Organic grown avocados ####\n")
-dt_organic = dt.query("type == 'organic'")
-years = dt_organic["year"].drop_duplicates()
-show_avg_price_each_year(dt_organic, "low", years)
+dt_organic = dt.query("type == 'organic'").copy()
+show_min_max(dt_conv, "Organic")
 print("###############\n")
 
-# highest avg price for both types each year
-print(
-    "#### Highest AVG Price Region for Organic and Convetionally grown avocados ####\n"
-)
-dt_both = get_data()
-years = dt_both["year"].drop_duplicates()
-show_avg_price_each_year(dt_both, "high", years)
-print("###############\n")
-# lowest all time price
+# # lowest all time price
 # for both coventionally and organic
 print(
     "#### Lowest AVG Price of all time for Organic and Convetionally grown avocados ####\n"
 )
-dt_low_all = get_data()
-years = dt_low_all["year"].drop_duplicates()
-print(show_avg_price_all_time(dt_low_all, "low", years))
+dt_both = dt.copy()
+years = dt_both["year"].drop_duplicates()
+print(show_avg_price_all_time(dt_both, "low", years))
 print("###############\n")
 # highest all time price
 # for both coventionally and organic
 print(
     "#### Highest AVG Price of all time for Organic and Convetionally grown avocados ####\n"
 )
-dt_high_all = get_data()
-print(show_avg_price_all_time(dt_high_all, "high", years))
+
+print(show_avg_price_all_time(dt_both, "high", years))
+
+## Simplified version
+highest_c = dt_both["AveragePrice"].max()
+print(f"Highest of both conventional and organic price simplified: {highest_c:.2f}")
